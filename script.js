89f94b77e1b7677db0f646cd89b8d1654d839f81
Vitoria Ibide <mvipirasol@gmail.com>
2026-05-05 16:03:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+        function salvarPlantio() {
+            const planta = {
+                cultura: document.getElementById('cultura').value,
+                data: document.getElementById('data-plantio').value,
+                local: "Horta Centro-Norte",
+                status: "Em crescimento"
+            };
+
+            console.log(planta);
+
+            if (!planta.data) {
+                alert("Por favor, selecione uma data!");
+                return;
+            }
+
+            // Pega o que já existe no "banco" ou cria uma lista vazia
+            const listaHortas = JSON.parse(localStorage.getItem('minhasHortas')) || [];
+            
+            // Adiciona a nova planta
+            listaHortas.push(planta);
+            
+            // Salva de volta no navegador
+            localStorage.setItem('minhasHortas', JSON.stringify(listaHortas));
+
+            alert("Plantio confirmado com sucesso!");
+            window.location.href = "painelcontrole.html"; // Vai para o painel
+        }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  
